00adb278df81c59518e9e7c63db8df6453fec5ba
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-04 12:40:39 -0500

    commit tree 8b6714ea5330530f35f7b463ec16657d01e80f4a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: %i[show new edit create update]
+  resources :posts, only: %i[show edit new create update]
 end
